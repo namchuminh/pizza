12f1bdc32d5bc667ad512c68a6ef75f95d9c6fa6
Nam Chu Minh <chuminhnamma@gmail.com>
2024-07-24 15:34:00 +0700

    thiet lap giao dien admin  va hoan thanh dang nhap admin

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,15 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use View;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {   
+        $api_url = env('API_URL');     
+        View::share('api_url', $api_url); 
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\AuthController;
+use App\Http\Controllers\Web\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
+Route::prefix('admin')->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('admin.login');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+});
 
