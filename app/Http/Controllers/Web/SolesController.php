6f0092f873adadfbd5c19133774b50c97294d338
Nam Chu Minh <chuminhnamma@gmail.com>
2024-07-25 15:17:45 +0700

    hoan thanh quan ly soles pizza

--- app/Http/Controllers/Web/SolesController.php
+++ app/Http/Controllers/Web/SolesController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class SolesController extends Controller
 {
-    //
+    public function index(){
+        return view('admin.soles.index');
+    }
+
+    public function create(){
+        return view('admin.soles.create');
+    }
+
+    public function update(){
+        return view('admin.soles.update');
+    }
 }
