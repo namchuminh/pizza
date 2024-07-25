95acb9ee8e1a1b8bd2e76c9517488ded4e653ce7
Nam Chu Minh <chuminhnamma@gmail.com>
2024-07-25 15:10:41 +0700

    hoan thanh quan ly border pizza

--- app/Http/Controllers/Web/BorderController.php
+++ app/Http/Controllers/Web/BorderController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class BorderController extends Controller
 {
-    //
+    public function index(){
+        return view('admin.border.index');
+    }
+
+    public function create(){
+        return view('admin.border.create');
+    }
+
+    public function update(){
+        return view('admin.border.update');
+    }
 }
